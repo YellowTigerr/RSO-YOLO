b1333bc04b0fdbccf8df7da1c2b6856eb9c80f5c
YellowTigerr <137086083+YellowTigerr@users.noreply.github.com>
2024-06-01 18:38:28 +0800

    Update augment block.py

--- obb/other_augment_block/augment block.py
+++ obb/other_augment_block/augment block.py
@@ -2,70 +2,12 @@ import numpy as np
 import cv2
 import os
 
-def singleScaleRetinex(img, sigma):
-    retinex = np.log10(img) - np.log10(cv2.GaussianBlur(img, (0, 0), sigma))
-
-    return retinex
-
-
-def multiScaleRetinex(img, sigma_list):
+def improve_Retinex(img, sigma_list,w1,w2,w3,radius):
+    img = np.float64(img) + 1.0
     retinex = np.zeros_like(img)
     for sigma in sigma_list:
-        retinex += singleScaleRetinex(img, sigma)
-
-    retinex = retinex / len(sigma_list)
-
-    return retinex
-
-
-def colorRestoration(img, alpha, beta):
-    img_sum = np.sum(img, axis=2, keepdims=True)
-
-    color_restoration = beta * (np.log10(alpha * img) - np.log10(img_sum))
-
-    return color_restoration
-
-
-def simplestColorBalance(img, low_clip, high_clip):
-    total = img.shape[0] * img.shape[1]
-    for i in range(img.shape[2]):
-        unique, counts = np.unique(img[:, :, i], return_counts=True)
-        current = 0
-        for u, c in zip(unique, counts):
-            if float(current) / total < low_clip:
-                low_val = u
-            if float(current) / total < high_clip:
-                high_val = u
-            current += c
-
-        img[:, :, i] = np.maximum(np.minimum(img[:, :, i], high_val), low_val)
-
-    return img
-
-
-def MSRCR(img, sigma_list, G, b, alpha, beta, low_clip, high_clip):
-    img = np.float64(img) + 1.0
-
-    img_retinex = multiScaleRetinex(img, sigma_list)
-    img_color = colorRestoration(img, alpha, beta)
-    img_msrcr = G * (img_retinex * img_color + b)
-
-    for i in range(img_msrcr.shape[2]):
-        img_msrcr[:, :, i] = (img_msrcr[:, :, i] - np.min(img_msrcr[:, :, i])) / \
-                             (np.max(img_msrcr[:, :, i]) - np.min(img_msrcr[:, :, i])) * \
-                             255
-
-    img_msrcr = np.uint8(np.minimum(np.maximum(img_msrcr, 0), 255))
-    img_msrcr = simplestColorBalance(img_msrcr, low_clip, high_clip)
-
-    return img_msrcr
-
-
-def automatedMSRCR(img, sigma_list):
-    img = np.float64(img) + 1.0
-
-    img_retinex = multiScaleRetinex(img, sigma_list)
-
+        retinex += np.log10(img) - np.log10(cv2.GaussianBlur(img, (0, 0), sigma))
+    img_retinex = retinex / len(sigma_list)
     for i in range(img_retinex.shape[2]):
         unique, count = np.unique(np.int32(img_retinex[:, :, i] * 100), return_counts=True)
         for u, c in zip(unique, count):
@@ -88,53 +30,39 @@ def automatedMSRCR(img, sigma_list):
                                (np.max(img_retinex[:, :, i]) - np.min(img_retinex[:, :, i])) \
                                * 255
 
+    img_retinex = np.float64(img_retinex)
+    G1 = cv2.GaussianBlur(img_retinex, (radius, radius), 1.0)
+
+    G2 = cv2.GaussianBlur(img_retinex, (radius * 2 - 1, radius * 2 - 1), 2.0)
+
+    G3 = cv2.GaussianBlur(img_retinex, (radius * 4 - 1, radius * 4 - 1), 4.0)
+
+
+    D1 = (1 - w1 * np.sign(img_retinex - G1)) * (img_retinex - G1)
+
+    D2 = w2 * (G1 - G2)
+
+    D3 = w3 * (G2 - G3)
+
+    img_mss = img_retinex + D1 + D2 + D3
+    img_retinex = cv2.convertScaleAbs(img_mss)
     img_retinex = np.uint8(img_retinex)
 
     return img_retinex
 
-def MSRCP(img, sigma_list, low_clip, high_clip):
-    img = np.float64(img) + 1.0
-
-    intensity = np.sum(img, axis=2) / img.shape[2]
-
-    retinex = multiScaleRetinex(intensity, sigma_list)
-
-    intensity = np.expand_dims(intensity, 2)
-    retinex = np.expand_dims(retinex, 2)
-
-    intensity1 = simplestColorBalance(retinex, low_clip, high_clip)
-
-    intensity1 = (intensity1 - np.min(intensity1)) / \
-                 (np.max(intensity1) - np.min(intensity1)) * \
-                 255.0 + 1.0
-
-    img_msrcp = np.zeros_like(img)
-
-    for y in range(img_msrcp.shape[0]):
-        for x in range(img_msrcp.shape[1]):
-            B = np.max(img[y, x])
-            A = np.minimum(256.0 / B, intensity1[y, x, 0] / intensity[y, x, 0])
-            img_msrcp[y, x, 0] = A * img[y, x, 0]
-            img_msrcp[y, x, 1] = A * img[y, x, 1]
-            img_msrcp[y, x, 2] = A * img[y, x, 2]
-
-    img_msrcp = np.uint8(img_msrcp - 1.0)
-
-    return img_msrcp
 
 
-def main(data_path)
+
+
+if __name__ == '__main__':
+    data_path = r'data'
     img_list = os.listdir(data_path)
-    retinex_path = os.path.join(os.path.dirname(data_path), 'retinex')
-    improve_retinex = os.path.join(os.path.dirname(data_path), 'improve_retinex')
     config = {
     "sigma_list": [15, 80, 250],
-    "G": 5.0,
-    "b": 25.0,
-    "alpha": 125.0,
-    "beta": 46.0,
-    "low_clip": 0.01,
-    "high_clip": 0.99
+    "w1": 0.5,
+    "w2": 0.5,
+    "w3": 0.25,
+    "radius": 3
     }
     if len(img_list) == 0:
         print
@@ -148,34 +76,16 @@ def main(data_path)
 
         img = cv2.imread(os.path.join(data_path, img_name))
 
-        img_msrcr = MSRCR(
+        img_enhence = improve_Retinex(
             img,
             config['sigma_list'],
-            config['G'],
-            config['b'],
-            config['alpha'],
-            config['beta'],
-            config['low_clip'],
-            config['high_clip']
+            config['w1'],
+            config['w2'],
+            config['w3'],
+            config['radius'],
         )
 
-        img_amsrcr = automatedMSRCR(
-            img,
-            config['sigma_list']
-        )
+        cv2.imshow('Image', img)
+        cv2.imshow('img_enhence', img_enhence)
+        cv2.waitKey()
 
-        img_msrcp = MSRCP(
-            img,
-            config['sigma_list'],
-            config['low_clip'],
-            config['high_clip']
-        )
-     
-        cv2.imwrite(os.path.join(retinex_path,img_name),img_msrcr)
-        cv2.imwrite(os.path.join(improve_retinex,img_name),img_amsrcr)
-
-
-if __name__ == '__main__':
-        path = "img_path"
-        main(path)
-        
