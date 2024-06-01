ccdbc350901dee45a0e8a748f81302ce6f6becad
YellowTigerr <137086083+YellowTigerr@users.noreply.github.com>
2024-06-01 18:41:01 +0800

    Update augment block.py

--- obb/other_augment_block/augment block.py
+++ obb/other_augment_block/augment block.py
@@ -2,6 +2,66 @@ import numpy as np
 import cv2
 import os
 
+def singleScaleRetinex(img, sigma):
+    retinex = np.log10(img) - np.log10(cv2.GaussianBlur(img, (0, 0), sigma))
+
+    return retinex
+
+
+def multiScaleRetinex(img, sigma_list):
+    retinex = np.zeros_like(img)
+    for sigma in sigma_list:
+        retinex += singleScaleRetinex(img, sigma)
+
+    retinex = retinex / len(sigma_list)
+
+    return retinex
+
+
+def colorRestoration(img, alpha, beta):
+    img_sum = np.sum(img, axis=2, keepdims=True)
+
+    color_restoration = beta * (np.log10(alpha * img) - np.log10(img_sum))
+
+    return color_restoration
+
+
+def simplestColorBalance(img, low_clip, high_clip):
+    total = img.shape[0] * img.shape[1]
+    for i in range(img.shape[2]):
+        unique, counts = np.unique(img[:, :, i], return_counts=True)
+        current = 0
+        for u, c in zip(unique, counts):
+            if float(current) / total < low_clip:
+                low_val = u
+            if float(current) / total < high_clip:
+                high_val = u
+            current += c
+
+        img[:, :, i] = np.maximum(np.minimum(img[:, :, i], high_val), low_val)
+
+    return img
+
+
+def MSRCR(img, sigma_list, G, b, alpha, beta, low_clip, high_clip):
+    img = np.float64(img) + 1.0
+
+    img_retinex = multiScaleRetinex(img, sigma_list)
+    img_color = colorRestoration(img, alpha, beta)
+    img_msrcr = G * (img_retinex * img_color + b)
+
+    for i in range(img_msrcr.shape[2]):
+        img_msrcr[:, :, i] = (img_msrcr[:, :, i] - np.min(img_msrcr[:, :, i])) / \
+                             (np.max(img_msrcr[:, :, i]) - np.min(img_msrcr[:, :, i])) * \
+                             255
+
+    img_msrcr = np.uint8(np.minimum(np.maximum(img_msrcr, 0), 255))
+    img_msrcr = simplestColorBalance(img_msrcr, low_clip, high_clip)
+
+    return img_msrcr
+
+
+
 def improve_Retinex(img, sigma_list,w1,w2,w3,radius):
     img = np.float64(img) + 1.0
     retinex = np.zeros_like(img)
@@ -59,6 +119,12 @@ if __name__ == '__main__':
     img_list = os.listdir(data_path)
     config = {
     "sigma_list": [15, 80, 250],
+    "G": 5.0,
+    "b": 25.0,
+    "alpha": 125.0,
+    "beta": 46.0,
+    "low_clip": 0.01,
+    "high_clip": 0.99,
     "w1": 0.5,
     "w2": 0.5,
     "w3": 0.25,
